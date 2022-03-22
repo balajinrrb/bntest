aad24a2abbb11e8c0cb651f44723115debb68283
balajinrrb <balajinrrb@outlook.com>
2022-03-22 12:42:15 +0530

    BNTEST Python Package installation from GitHub Repo!

---
+++ BNTEST/__init__.py
@@ -0,0 +1,2 @@
+def bn_test_method():
+    print("BN Testing Package Working Fine!!")
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='BNTEST',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='git@github.com:balajinrrb/bntest.git',
+    author='Balaji N',
+    author_email='balajinrrb@gmail.com',
+    license='MIT License',
+    packages=['BNTEST'],
+    zip_safe=False
+)
